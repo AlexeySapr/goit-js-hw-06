354109e9fa403c94454e3ca6b9024a3ae8f87685
Alexey Sapr <alexeysapr@gmail.com>
2021-10-03 18:34:15 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+console.log(list);
+
+for (const ingredient of ingredients) {
+	const item = document.createElement('li');
+	item.textContent = ingredient;
+	item.classList.add('item');
+	list.append(item);
+}
