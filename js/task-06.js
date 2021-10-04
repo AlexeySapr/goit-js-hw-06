fb11edd4490017b534d5c0f9b994d5a3cd84e761
Alexey Sapr <alexeysapr@gmail.com>
2021-10-04 22:01:27 +0300

    change task-03, task-06

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,11 @@ const textInput = document.querySelector('input#validation-input');
 textInput.addEventListener('blur', (event) => {
 	const attrValue = textInput.getAttribute('data-length');
 
-	textInput.value.length !== Number(attrValue)
-		? textInput.classList.add('invalid')
-		: textInput.classList.replace('invalid', 'valid');
+	if (textInput.value.length === Number(attrValue)) {
+		textInput.classList.add('valid');
+		textInput.classList.replace('invalid', 'valid');
+	} else {
+		textInput.classList.add('invalid');
+		textInput.classList.replace('valid', 'invalid');
+	}
 });
