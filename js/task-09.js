72b7f965c8e4d55bccb4d122ff470062fbc00d6c
Alexey Sapr <alexeysapr@gmail.com>
2021-10-05 14:17:05 +0300

    add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+const colorText = document.querySelector('span.color');
+
+changeColorBtn.addEventListener('click', (event) => {
+	const someColor = getRandomHexColor();
+	body.style.backgroundColor = someColor;
+	colorText.textContent = someColor;
+});
