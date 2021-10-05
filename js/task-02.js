42a04862f92551d82248039d0d94108a66d3cad8
Alexey Sapr <alexeysapr@gmail.com>
2021-10-05 15:35:56 +0300

    add  task-10

--- js/task-02.js
+++ js/task-02.js
@@ -1,11 +1,24 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
 const list = document.querySelector('ul#ingredients');
-console.log(list);
+// console.log(list);
+
+/* первый способ */
+// for (const ingredient of ingredients) {
+// 	const item = document.createElement('li');
+// 	item.textContent = ingredient;
+// 	item.classList.add('item');
+// 	list.append(item);
+// }
+
+/* второй способ */
+const liItems = [];
 
 for (const ingredient of ingredients) {
 	const item = document.createElement('li');
 	item.textContent = ingredient;
 	item.classList.add('item');
-	list.append(item);
+	liItems.push(item);
 }
+
+list.append(...liItems);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,45 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// получаем ссылки на необходимые элементы
+const boxesField = document.getElementById('boxes');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+
+const controlsBox = document.querySelector('div#controls');
+const inputField = controlsBox.firstElementChild;
+
+//функция createBoxes()
+function createBoxes(amount) {
+	let width = 30;
+	let height = 30;
+
+	for (let i = 1; i < amount + 1; ++i) {
+		const box = document.createElement('div');
+		// box.textContent = i;
+		box.classList.add('box');
+		box.style.width = width + 'px';
+		box.style.height = height + 'px';
+		box.style.backgroundColor = getRandomHexColor();
+		boxesField.append(box);
+		width += 10;
+		height += 10;
+	}
+}
+
+//функция destroyBoxes()
+function destroyBoxes() {
+	boxesField.innerHTML = '';
+}
+
+//создание блоков div
+createBtn.addEventListener('click', (event) => {
+	const numBoxes = Number(inputField.value);
+	createBoxes(numBoxes);
+});
+
+//удаление блоков div
+destroyBtn.addEventListener('click', () => {
+	destroyBoxes();
+});
