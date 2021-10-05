9929dbb9d9150d06e0fb71209e3e7014528b9af8
Alexey Sapr <alexeysapr@gmail.com>
2021-10-05 12:01:27 +0300

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('form.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+	event.preventDefault();
+	// console.log(event.currentTarget.elements);
+
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === '' || password.value === '') {
+		alert('Пожалуйста заполните все поля формы');
+	} else {
+		console.log({ email: email.value, password: password.value });
+		event.currentTarget.reset();
+	}
+}
